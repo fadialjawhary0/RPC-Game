49101ebc2d02649e05170cba2aec74b6ae63195e
fadialjohari0 <fadialjohari0@gmail.com>
2023-12-02 22:56:24 +0300

    Add assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import { CssBaseline } from "@mui/material";
+import RPC from "./views/RPC/components/RPC";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <CssBaseline></CssBaseline>
+      <CssBaseline>
+        <RPC />
+      </CssBaseline>
     </BrowserRouter>
   );
 };
