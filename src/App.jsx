0b1ae3123be7abd4a912570b56cc3d7c6f0ec61b
fadialjohari0 <fadialjohari0@gmail.com>
2023-12-02 22:27:35 +0300

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 
 import { CssBaseline } from "@mui/material";
 
-import AppContainer from "./layouts/index";
-
 export const App = () => {
   return (
     <BrowserRouter>
-      <CssBaseline>
-        <AppContainer />
-      </CssBaseline>
+      <CssBaseline></CssBaseline>
     </BrowserRouter>
   );
 };
