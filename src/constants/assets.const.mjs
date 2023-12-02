ca6a3c120ace2e084d9980bd653e67e91ef62d42
fadialjohari0 <fadialjohari0@gmail.com>
2023-12-03 02:06:58 +0300

    Create static header

---
+++ src/constants/assets.const.mjs
@@ -0,0 +1,38 @@
+import Rock from "../assets/icon-rock.svg";
+import Paper from "../assets/icon-paper.svg";
+import Scissors from "../assets/icon-scissors.svg";
+import CloseIcon from "../assets/icon-close.svg";
+import Triangle from "../assets/bg-triangle.svg";
+import Rules from "../assets/image-rules.svg";
+import Logo from "../assets/logo.svg";
+
+export const Assets = {
+  rock: {
+    name: "Rock",
+    icon: Rock,
+  },
+  paper: {
+    name: "Paper",
+    icon: Paper,
+  },
+  scissors: {
+    name: "Scissors",
+    icon: Scissors,
+  },
+  closeIcon: {
+    name: "Close Icon",
+    icon: CloseIcon,
+  },
+  triangle: {
+    name: "Triangle Background",
+    icon: Triangle,
+  },
+  rules: {
+    name: "Rules",
+    icon: Rules,
+  },
+  logo: {
+    name: "Logo",
+    icon: Logo,
+  },
+};
---
+++ src/views/RPC/components/Header.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { Grid, Typography } from "@mui/material";
+
+import { Assets } from "../../../constants/assets.const.mjs";
+
+const Header = () => {
+  return (
+    <Grid
+      container
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{
+        maxWidth: "50rem",
+        width: "100%",
+        height: "8rem",
+        padding: "1rem",
+        borderRadius: 4,
+        border: "2px hsl(217, 16%, 45%) solid",
+      }}
+    >
+      <Grid
+        item
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          width: { xs: "7rem", sm: "10rem" },
+        }}
+      >
+        <img
+          src={Assets.logo.icon}
+          alt={Assets.logo.name}
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid
+        item
+        sx={{
+          width: { xs: "6rem", sm: "8rem" },
+          height: "100%",
+          backgroundColor: "white",
+          borderRadius: 2,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "hsl(229, 64%, 46%)",
+          }}
+        >
+          SCORE
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "3rem",
+            lineHeight: 1,
+            color: "hsl(229, 25%, 31%)",
+          }}
+        >
+          12
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Header;
